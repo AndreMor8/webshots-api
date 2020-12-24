88e9f33d122e74a464b95fcb8b81df8aa2c23292
AndreMor955 <moralesandre75@gmail.com>
2020-12-23 21:41:14 -0500

    Initial commit

---
+++ clean-url/index.js
@@ -0,0 +1,32 @@
+const data = require('./assets/sites.json');
+const isporncallback = require("is-porn");
+const { promisify } = require("util");
+const isPorn = promisify(isporncallback);
+// methods to be exported
+module.exports.checkCleanUrl = (urlFromReq) => {
+    let parsedUrl = new URL(urlFromReq);
+    let host = parsedUrl.host || parsedUrl.pathname.trim().split("/")[0];
+    if (host in data) return true;
+    let thing = host.split(".");
+    let check1 = thing.slice(thing.length - 2).join(".");
+    let check2 = "www." + check1;
+    if (check2 in data) return true;
+    if (!host.startsWith('www.')) host = `www.${host}`;
+    if (host in data) return true;
+    return false;
+}
+
+module.exports.checkSingleCleanURL = async (urlFromReq) => {
+    let parsedUrl = new URL(urlFromReq);
+    let host = parsedUrl.host || parsedUrl.pathname.trim().split("/")[0];
+    if (host in data) return true;
+    let thing = host.split(".");
+    let check1 = thing.slice(thing.length - 2).join(".");
+    let check2 = "www." + check1;
+    if (check2 in data) return true;
+    if (!host.startsWith('www.')) host = `www.${host}`;
+    if (host in data) return true;
+    const cosas = await isPorn(check1);
+    if (cosas) return true;
+    return false;
+}
---
+++ server.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+const puppeteer = require("puppeteer");
+const express = require("express");
+const deepai = require("deepai");
+const { checkSingleCleanURL } = require("./clean-url/index.js");
+deepai.setApiKey(process.env.DEEPAI);
+const app = express();
+app.use(express.json());
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: true, defaultViewport: {
+            width: parseInt(process.env.WIDTH),
+            height: parseInt(process.env.HEIGHT)
+        }, args: ["--disable-gpu", "--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
+        executablePath: process.env.CHROME_BIN || null
+    });
+    app.use((req, res, next) => {
+        if (req.headers["auth-token"] === process.env.TOKEN) next();
+        else res.status(403).send("Nope no puedes usar esta API");
+    })
+
+    app.get("/", (req, res) => {
+        res.status(200).json({
+            "author": "AndreMor#0008",
+            "method": "/ss"
+        });
+    });
+
+    app.post("/ss", async (req, res) => {
+        try {
+            if (!req.body.url) return res.status(400).send("Oye manda un URL primero");
+            if (!/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/gm.test(req.body.url)) return res.status(400).send("This is a invalid URL.");
+            if (!req.body.nsfw) {
+                if (await checkSingleCleanURL(req.body.url)) return res.status(401).send("NSFW content has been detected in the generated image. If you want to see it, ask for it on a NSFW channel.");
+            }
+            const page = await browser.newPage();
+            await page.goto(req.body.url, { waitUntil: "networkidle2" });
+            const options = { x: req.body.x, y: req.body.y };
+            if (options && !isNaN(options.x) && !isNaN(options.y)) {
+                screenshot = await page.screenshot({
+                    clip: { x: parseInt(options.x), y: parseInt(options.y), width: parseInt(process.env.WIDTH), height: parseInt(process.env.HEIGHT) },
+                    type: "png"
+                });
+            } else {
+                screenshot = await page.screenshot({ type: "png" });
+            }
+
+            if (!req.body.nsfw) {
+                const results = await deepai.callStandardApi("nsfw-detector", { image: screenshot });
+                if (results.output.nsfw_score > 0.5) return res.status(401).send("NSFW content has been detected in the generated image. If you want to see it, ask for it on a NSFW channel.");
+            }
+            res.set({ 'Content-Type': 'image/png' });
+            res.send(screenshot);
+        } catch (err) {
+            res.status(500).send("Some error ocurred! " + err.toString());
+        }
+    });
+
+    app.use("/ss", (req, res) => res.status(405).send("Oye no seas loco es con un POST"));
+
+    const listener = app.listen(process.env.PORT, () => {
+        console.log("Listening on port " + listener.address().port);
+    });
+})();
